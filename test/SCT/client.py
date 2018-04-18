15f363fee56467fec89951b6768186918a9b43ce
Michal <marciniak.wro@gmail.com>
2018-04-18 14:53:12 +0200

    Added possibility in scts to wait unitl server starts

--- test/SCT/client.py
+++ test/SCT/client.py
@@ -9,8 +9,11 @@ class ClientStub:
   def run(self):
     channel = grpc.insecure_channel('localhost:50051')
     stub = casisco_pb2_grpc.CasiscoStub(channel)
-    response = stub.registerUser(casisco_pb2.UserRegisterInfo(name="dupa", password="dupa_password", email="dupa@mail.com"))
+    
+    response = stub.registerUser(casisco_pb2.UserRegisterInfo(
+        name="test", password="password", email="test@mail.com"))
     print("Greeter client received: {}".format(response.status))
 
-    response = stub.loginUser(casisco_pb2.UserLoginInfo(name="dupa", password="dupa_password"))
+    response = stub.loginUser(casisco_pb2.UserLoginInfo(
+        name="test", password="password"))
     print("Greeter client received: {}".format(response.status))
