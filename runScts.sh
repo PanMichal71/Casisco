a87dcd1b6e717eb72b344cbe5a05a8dd07103c9c
Michal <marciniak.wro@gmail.com>
2018-08-17 10:49:19 +0200

    Added virtualenv support in runScts script

--- runScts.sh
+++ runScts.sh
@@ -1,3 +1,9 @@
+python -m virtualenv venv
+source venv/bin/activate
+pip install grpcio
+pip install grpcio-tools
+pip install pytest
+
 PATH=$PATH:$PWD/build/src/server
 echo "Generating python source files"
 CASISCO_GRPC_DIR="./test/SCT/casisco_grpc"
